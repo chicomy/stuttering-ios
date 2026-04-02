6f7d36015ce369c4002f7c0a83c4df2082925105
chicomy <chongmiw@gmail.com>
2026-04-02 15:53:12 +0800

    Initial commit

---
+++ stuttering app/stuttering app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stuttering app
+//
+//  Created by Chongming Wang on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stuttering app/stuttering app/stuttering_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  stuttering_appApp.swift
+//  stuttering app
+//
+//  Created by Chongming Wang on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct stuttering_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
